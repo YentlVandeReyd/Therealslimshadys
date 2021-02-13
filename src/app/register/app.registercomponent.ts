05b9ef42ad6d66105b85a1a46ff75b8463eae566
hagen <hagen_draulans@hotmail.com>
2021-02-13 03:22:36 +0100

    register form code + mappen +foto

---
+++ src/app/register/app.registercomponent.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { title } from 'process';
+
+
+
+@Component({
+    selector: 'app-register',
+    templateUrl: './app.register.component.html',
+    styleUrls: ['./app.register.component.css']
+})
+export class registerComponent {
+    title = 'register';
+
+    
+}
