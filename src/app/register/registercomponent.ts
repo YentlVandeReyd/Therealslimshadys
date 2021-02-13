e2623e3279d8ae2959c1e523afa96aaf1e043ce2
hagen <hagen_draulans@hotmail.com>
2021-02-14 00:32:54 +0100

    register component aangepast + shared file

--- src/app/register/registercomponent.ts
+++ src/app/register/registercomponent.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { title } from 'process';
-
+import { Todo} from '../../app/shared/todo.model';
 
 
 @Component({
     selector: 'app-register',
-    templateUrl: './app.register.component.html',
-    styleUrls: ['./app.register.component.css']
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class registerComponent {
     title = 'register';
